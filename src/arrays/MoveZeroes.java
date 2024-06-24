72dcb93911edba13efef734d06f3ef2b0c590c30
Abhishikth <abhishikth.karumanchi@kelltontech.com>
2024-06-24 18:05:45 +0530

    Added gitignore and removed class files

--- src/arrays/MoveZeroes.java
+++ src/arrays/MoveZeroes.java
@@ -2,4 +2,25 @@ package arrays;
 
 public class MoveZeroes {
 
+	public static void main(String[] args) {
+		MoveZeroes obj = new MoveZeroes();
+
+		ArrayMain.printIntArray(obj.moveZeroes(new int[] { 0, 1, 0, 3, 12 }));
+		ArrayMain.printIntArray(obj.moveZeroes(new int[] { 0 }));
+		ArrayMain.printIntArray(obj.moveZeroes(new int[] { 0, 1, 0, 0, 3 }));
+	}
+
+	public int[] moveZeroes(int[] nums) {
+		int left = 0;
+		for (int right = 0; right < nums.length; right++) {
+			if (nums[right] != 0) {
+				int temp = nums[left];
+				nums[left] = nums[right];
+				nums[right] = temp;
+				left++;
+			}
+		}
+		return nums;
+	}
+
 }
