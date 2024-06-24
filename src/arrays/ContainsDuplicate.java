0904e3054b3872ecbef53582cb367b193798f69d
Abhishikth <abhishikth.karumanchi@kelltontech.com>
2024-06-24 18:15:02 +0530

    Added contains duplicate

--- src/arrays/ContainsDuplicate.java
+++ src/arrays/ContainsDuplicate.java
@@ -1,5 +1,28 @@
 package arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
 
+	public static void main(String[] args) {
+
+		ContainsDuplicate obj = new ContainsDuplicate();
+		System.out.println(obj.containsDuplicate(new int[] { 1, 2, 3, 1 }));
+		System.out.println(obj.containsDuplicate(new int[] { 1, 2, 3, 4 }));
+		System.out.println(obj.containsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
+	}
+
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (set.contains(nums[i])) {
+				return true;
+			} else {
+				set.add(nums[i]);
+			}
+		}
+		return false;
+
+	}
 }
--- src/arrays/MaximumSubarray.java
+++ src/arrays/MaximumSubarray.java
@@ -1,32 +1,14 @@
 package arrays;
 
-import java.util.Arrays;
-
 public class MaximumSubarray {
 
 	public static void main(String[] args) {
-		//{5,4,-1,7,8}
-		// -1, 4, 5, 7, 8
-		/*
-		 * cs = 8
-		 * 
-		 */
 		MaximumSubarray obj = new MaximumSubarray();
-		System.out.println(obj.maxSubArray(new int[] {5,4,-1,7,8}));
+		System.out.println(obj.maxSubArray(new int[] { 5, 4, -1, 7, 8 }));
 	}
-	
+
 	public int maxSubArray(int[] nums) {
-		Arrays.sort(nums);
-		int currentSum = 0;
-		int nextSum = nums[nums.length-1];
-		for(int i = nums.length-1; i >= 0; i--) {
-			currentSum += nums[i];
-			nextSum = currentSum + nums[i-1];
-			if(nextSum<currentSum) {
-				return currentSum;
-			}
-		}
-		
 		return -1;
 	}
+
 }
